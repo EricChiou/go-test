103317525fd4ca571a0d9c2256b1ce1fbcf877a8
Chiu <Eric.Chiu@acereb.com>
2019-11-01 16:05:06 +0800

    add test goroutine pool

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/EricChiou/go-test
+
+go 1.13
+
+require github.com/EricChiou/goroutinepool v0.0.0-20191101075012-38513f9069c0
--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/EricChiou/goroutinepool"
+)
+
 func main() {
+	// test goroutine pool
+	// testGoroutinepool()
+
+	// test http router
+	testHttprouter()
+}
+
+func testGoroutinepool() {
+	pool := goroutinepool.New(5) // new gorotine pool has 5 gorotine
+	for i := 0; i < 1000; i++ {
+		pool.Add()
+		go func(i int) {
+			fmt.Println("gorotine ", i)
+			pool.Done()
+		}(i)
+	}
+	pool.Wait()
+}
+
+func testHttprouter() {
 
 }
